4952bd73b7284cc2da05ceebd3d2e90be6f0b0fe
31010-83-06 <31010-83-06@STA6300008.exchange.ad.afpanet>
2021-01-12 17:03:48 +0100

    Redéfinition de l'objet Element
    Initialisation de l'attribut à false pour dire qu'on ne veux pas que
    l'élément crée est touché

--- LG_java_EDC/src/flotte/Element.java
+++ LG_java_EDC/src/flotte/Element.java
@@ -7,23 +7,16 @@ public class Element {
 	private int positionX;
 	private int positionY;
 	private Bateau bateau;
-	private boolean bTouche;
+	private boolean bTouche = false;
 	
 
 
-	//Getter
-	int getPositionX() {
-		return positionX;
-	}
-	public int getPositionY() {
-		return positionY;
-	}
-	public boolean isbTouche() {
-		return bTouche;
-	}
+
 	
 	
 	
+
+
 	public Element(int positionX, int positionY, Bateau bateau) {
 		this.positionX = positionX;
 		this.positionY = positionY;
@@ -37,5 +30,38 @@ public class Element {
 	
 	public  void avancer(int depX, int depY) {
 		 
+	}
+	
+	
+	public int getPositionX() {
+		return positionX;
+	}
+
+	public void setPositionX(int positionX) {
+		this.positionX = positionX;
+	}
+
+	public int getPositionY() {
+		return positionY;
+	}
+
+	public void setPositionY(int positionY) {
+		this.positionY = positionY;
+	}
+
+	public Bateau getBateau() {
+		return bateau;
+	}
+
+	public void setBateau(Bateau bateau) {
+		this.bateau = bateau;
+	}
+
+	public boolean isbTouche() {
+		return bTouche;
+	}
+
+	public void setbTouche(boolean bTouche) {
+		this.bTouche = bTouche;
 	}
 }
