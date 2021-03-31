40b23b1b34b497b975d52d7e4ffd4bd4a31fe9f4
Lucas <lucas.griggio@gmail.com>
2021-03-31 18:59:30 +0200

    clean launcher

--- LG_java_EDC/src/principal/Lanceur.java
+++ LG_java_EDC/src/principal/Lanceur.java
@@ -11,16 +11,7 @@ import utilitaire.Saisie;
 public class Lanceur {
 
 	public static void main(String[] args) {
-		final int TAILLEMINGRILLE = 10;
-		final int NOMBREMINBATEAU = 3;
-
-
-
-		int tailleGrille = 0;
-		boolean isTailleOK = false;
-		int nombreBateau = 0;
-		boolean isNombreBateauOk = false;
-		boolean isHorizontalBool = false;
+	
 
 		System.out.println("---------Bataille Navale------------");
 		
